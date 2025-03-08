4b96f21ff7b9562f562016d187bea7a3fb1872a6
WALSSIMON DOS SANTOS SILVA SACRAMENTO <walssimon.sssacramen@senacsp.edu.br>
2025-03-07 21:32:37 -0300

    stack

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,19 @@
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
-    Pilha<Integer> pilha = new Pilha<Integer>();
+        Stack<Integer> stack = new Stack<Integer>();
 
 
-        pilha.empilhar(1);
-        pilha.empilhar(2);
-        pilha.empilhar(3);
-        System.out.println("Desempilhar elemento " + pilha.desempilha());
-        System.out.println(pilha);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        System.out.println(stack.size());
+        System.out.println(stack);
+        System.out.println(stack.peek());
+        System.out.println(stack.pop());
+        System.out.println(stack);
+
         }
 
 }
